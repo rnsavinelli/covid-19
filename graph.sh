ff076cf829e65daf3777624bae35ec2964a6fdaf
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-07-28 18:43:53 -0300

    fixed bad behaviour

--- graph.sh
+++ graph.sh
@@ -13,12 +13,11 @@ listandReturn() {
         *) ls -drc "$1"/*/ | awk -F '/' '{print $3}' | nl ;;
     esac
     read -erp "Pick an entry by number to plot, or press Ctrl-C to cancel. " number
+    [[ "$number" -lt "1" ]] || [[ "$number" -gt "$(ls -d "$1"/*/ | wc -l)" ]] && printf "Error: Invalid selection.\\nAborting execution.\\n" && exit 1
     selection="$(ls -drc "$1"/*/ | awk -F '/' '{print $3}' | nl | grep -w "$number" | awk '{print $2}')"
 }
 
 listandReturn $dir
-#[[ "$selection" -lt "1" ]] || [[ "$selection" -gt "$(ls -d "$1"/*/ | wc -l)" ]] && printf "Error: Invalid selection.\\nAborting execution.\\n" && exit 1
-
 printf "Rscript graph.R ./dashboards/%s/dashboard.csv:\\n" $selection
 Rscript graph.R ./dashboards/$selection/dashboard.csv
 printf "Saving files to %s/%s... " $dir $selection
